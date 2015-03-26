43638193c152a9fca198b2f6e98a1cae77725362
Pete Nellius <pete.nellius@ogilvy.com>
2015-03-26 14:24:25 -0400

    Almost done the exercise

--- src/Lesson02/FourthViewController.swift
+++ src/Lesson02/FourthViewController.swift
@@ -9,16 +9,40 @@
 import UIKit
 
 class FourthViewController: UIViewController {
+    
+    @IBOutlet weak var numberField: UITextField!
+    @IBOutlet weak var fibLabel: UILabel!
+    let numberEntered = numberField.text.toInt() ?? fibLabel.text = "You need to enter a value"
+    @IBAction func calculateFib(sender: AnyObject) {
+        if numberEntered == true {
+        FibonacciAdder.fibonacciNumberAtIndex(numberEntered)
+        } else {
+        fibLabel.text = "You need to enter a value"
+        }
+    }
+    
+    
 /*
     TODO seven: Hook up the text input box, label and and a ‘calculate’ button. Create a ‘fibonacci adder’ class with a method ‘fibonacciNumberAtIndex' which takes indexOfFibonacciNumber (an integer).  When the button is pressed, create an instance of that class, call the method, and print out the appropriate fibonacci number of an inputted integer.
     The first fibonacci number is 0, the second is 1, the third is 1, the fourth is two, the fifth is 3, the sixth is 5, etc. The Xth fibonacci number is the sum of the X-1th fibonacci number and the X-2th fibonacci number.
 */
     
-    // Initial number variable
-    // Increment that variable by 1 initially
-    // Loop
-    for 
-    // Add previously returned variable by last number
     
-    
+    class FibonacciAdder {
+        
+        
+        func fibonacciNumberAtIndex (indexOfFibonacciNumber: Int) {
+            var fibArray = [0,1]
+            for var j = 0; j < indexOfFibonacciNumber; j++ {
+                var n1 = fibArray[fibArray.count - 1]
+                var n2 = fibArray[fibArray.count - 2]
+                var n3 = n1+n2
+                fibArray.append(n3)
+            }
+            
+        }
+        
+        
+    }
+
 }
